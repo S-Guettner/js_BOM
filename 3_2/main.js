7f3fbecc3d79321523d69fdf81a24a4e78fdbeae
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-15 17:32:56 +0100

    skips full minutes seconds display direct  59 instead of 00

--- 3_2/main.js
+++ 3_2/main.js
@@ -3,21 +3,48 @@ const time = document.querySelector("#time")
 
 let minutes
 let seconds = 60
-let counter
-let interval
+
+let intervalSeconds
+let intervalMinutes
 
 const startMinCountdown = () => {
-    minutes = Number(minutesInput.value)
+    minutes = parseInt(minutesInput.value-1)
 
+    
+    if(seconds <= 0 ){seconds += 59}
+
+    
     intervalSeconds = setInterval(() => {
         seconds--
-        counter++
-        time.innerHTML = `${minutes}:${seconds}`
-        console.log(`${minutes}:${seconds}`)
-    }, 1000);
+        if(seconds > 59){seconds = 0}
+        if(seconds <= 0 && minutes > 0 ){seconds += 59}
+        if(seconds < 10 ){
+            time.innerHTML = `${minutes}:0${seconds}`
+        }else{
+            time.innerHTML = `${minutes}:${seconds}`
+        }
+        if(seconds < 1 ) {
+            clearInterval(intervalSeconds)
+        }
+        console.log(seconds)
+    },1000)
+
     intervalMinutes = setInterval(() => {
         minutes--
         time.innerHTML = `${minutes}:${seconds}`
-        console.log(`${minutes}:${seconds}`)
-    }, 60000);
+    },60000)
+    
+    if (minutes < 1 ) {
+        clearInterval(intervalMinutes)
+    }
+
 }   
+
+const pauseMinCountdown = () => {
+    clearInterval(intervalMinutes)
+    clearInterval(intervalSeconds)
+}
+
+const restartMinCountdown = () => {
+    startMinCountdown()
+}
