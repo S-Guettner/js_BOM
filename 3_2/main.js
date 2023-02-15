5b64b7840d8568a2caf09444c4e38ba63354d921
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-15 20:38:24 +0100

    finish

--- 3_2/main.js
+++ 3_2/main.js
@@ -2,23 +2,32 @@ const minutesInput = document.querySelector("#minutes")
 const time = document.querySelector("#time")
 
 let minutes = 0
-let seconds = 60
+let seconds = 0
 
 let intervalSeconds
 let intervalMinutes
 
 const startMinCountdown = () => {
-    minutes = Number(minutesInput.value-1)
+    minutes = Number(minutesInput.value)
     console.log(minutes)
+
+    if(seconds === 0 ){
+        time.innerHTML = `${minutes}:00`
+        seconds += 60
+        minutes--
+    }
 
     intervalSeconds = setInterval(() => {
         seconds--
         console.log(seconds)
         console.log(minutes)
+        if(minutes === 0 && seconds === 0){
+            clearInterval(intervalSeconds)
+        }
         if(seconds == 0 ){
             time.innerHTML = `${minutes}:00`
-            minutes--
             seconds += 60
+            minutes--
         }else if(seconds < 10){
             time.innerHTML = `${minutes}:0${seconds}`
         }
@@ -26,10 +35,12 @@ const startMinCountdown = () => {
             time.innerHTML = `${minutes}:${seconds}`
         }
     },1000)
+
+
 }
 
 const pauseMinCountdown = () => {
-    clearInterval(intervalMinutes)
+
     clearInterval(intervalSeconds)
 }
 
